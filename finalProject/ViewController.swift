b67c80b476431b026a4ed37699f6b6071632ae9a
axm862 <axm862@case.edu>
2018-04-09 14:15:16 -0400

    Initial Commit

---
+++ finalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  finalProject
+//
+//  Created by Aditya Malik on 4/9/18.
+//  Copyright © 2018 Malik and Walls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
